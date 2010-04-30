7ecdc695caec91ff963e58180ef583ccdc15a732
hosimitu <hosimitu@gmail.com>
2010-04-30 14:14:05 +0900

    OAuth Library by abraham. http://github.com/abraham/twitteroauth

---
+++ setting.php
@@ -0,0 +1,10 @@
+<?php
+//Consumer keyの値
+$consumer_key = "consumer_key";
+//Consumer secretの値
+$consumer_secret = "consumer_secret";
+//Access Tokenの値
+$access_token = "access_token";
+//Access Token Secretの値
+$access_token_secret = "access_token_secret";
+?>
